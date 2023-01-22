07c0ab61a292575922bd0fbe0287c73e4890bf63
Egor Sosulin <egorsosulin@gmail.com>
2023-01-22 17:39:29 +0300

    Switch to Poetry and add RPM build

---
+++ build.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+
+CONTAINER_CMD="sudo podman"
+
+
+function build-bin {
+    container="fedora:37"
+
+    $CONTAINER_CMD run --rm -ti -v $(pwd):/repo -w /repo $container /bin/bash -c "
+        dnf -y install python3-devel python3-pip poetry && \
+        dnf -y group install 'C Development Tools and Libraries' && \
+        \
+        pip3 install pyinstaller && \
+        \
+        poetry export | pip3 install -r /dev/stdin && \
+        \
+        pyinstaller --noconfirm --onefile \
+            --collect-data beancount \
+            --collect-submodules beancount \
+            --name moneyctl \
+            --workpath /build \
+            --distpath /dist \
+            --specpath /spec \
+            moneyctl/__init__.py && \
+        \
+        mv /dist/moneyctl /repo/moneyctl.bin && \
+        chown $(id -u):$(id -g) /repo/moneyctl.bin"
+}
+
+function build-rpm {
+    container="fedora:37"
+
+    $CONTAINER_CMD run --rm -ti -v $(pwd):/repo -w /repo $container /bin/bash -c "\
+        dnf -y install rpmdevtools && \
+        \
+        rpmdev-setuptree && \
+        cp /repo/moneyctl.bin /root/rpmbuild/BUILD/moneyctl && \
+        cp /repo/moneyctl.rpm.spec /root/rpmbuild/SPECS/moneyctl.spec && \
+        \
+        rpmbuild -bb /root/rpmbuild/SPECS/moneyctl.spec && \
+        \
+        mv /root/rpmbuild/RPMS/x86_64/*.rpm /repo/ && \
+        chown $(id -u):$(id -g) /repo/*.rpm"
+}
+
+
+function main {
+    case $1 in
+        bin)
+            build-bin
+            ;;
+        rpm)
+            [[ -f "moneyctl.bin" ]] || build-bin
+            build-rpm
+            ;;
+        clean)
+            rm -f *.bin *.rpm
+            ;;
+        help)
+            echo "./build.sh (bin|rpm|clean)"
+            ;;
+    esac
+}
+
+main $*
---
+++ moneyctl.rpm.spec
@@ -0,0 +1,17 @@
+Name:           moneyctl
+Version:        0.1.0
+Release:        1%{?dist}
+License:        GPL
+Summary:        CLI finance management application based on Beancount
+
+%description
+CLI finance management application based on Beancount
+
+
+%install
+mkdir -p %{buildroot}%{_bindir}
+install -m 755 %{name} %{buildroot}%{_bindir}/%{name}
+
+
+%files
+%{_bindir}/%{name}
